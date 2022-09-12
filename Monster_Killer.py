4cb555863b759960aa8a576efac9f32263918465
kurtissfrost <113222022+kurtissfrost@users.noreply.github.com>
2022-09-11 23:34:26 -0500

    Add files via upload

---
+++ Monster_Killer.py
@@ -0,0 +1,34 @@
+#This project is a test of a text based "combat" system using a RNG
+#This is the promt that is setting up the story before combat
+
+print ("""███╗░░░███╗░█████╗░███╗░░██╗░██████╗████████╗███████╗██████╗░  ██╗░░██╗██╗██╗░░░░░██╗░░░░░███████╗██████╗░
+████╗░████║██╔══██╗████╗░██║██╔════╝╚══██╔══╝██╔════╝██╔══██╗  ██║░██╔╝██║██║░░░░░██║░░░░░██╔════╝██╔══██╗
+██╔████╔██║██║░░██║██╔██╗██║╚█████╗░░░░██║░░░█████╗░░██████╔╝  █████═╝░██║██║░░░░░██║░░░░░█████╗░░██████╔╝
+██║╚██╔╝██║██║░░██║██║╚████║░╚═══██╗░░░██║░░░██╔══╝░░██╔══██╗  ██╔═██╗░██║██║░░░░░██║░░░░░██╔══╝░░██╔══██╗
+██║░╚═╝░██║╚█████╔╝██║░╚███║██████╔╝░░░██║░░░███████╗██║░░██║  ██║░╚██╗██║███████╗███████╗███████╗██║░░██║
+╚═╝░░░░░╚═╝░╚════╝░╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚══════╝╚═╝░░╚═╝  ╚═╝░░╚═╝╚═╝╚══════╝╚══════╝╚══════╝╚═╝░░╚═╝""")
+print ("Created and Coded by Kurtiss Frost")
+print ("Version 0.0.1a Alpha Build")
+input ("Press ENTER to set out on your adventure!")
+print ("------------------------------------------------------------------------------------------------------------------------------")
+print ("> You are Emarim, a wizard living in the kingdom of Larion.")
+print ("> You have a staff, a sword and a spellbook. You finish your long journey and finally arrive at the ruin you've been looking for.")
+print ("> You have come here searching for a mystical spellbook of great power called the book of essence.")
+print ("> You look around and see no other people here. There is nothing but the great desert stretching into the distance and the ruin itself.")
+print ("> Suddenly you feel something amazing...it is a portal opening up.")
+print ("> The portal is a great swirling portal of light that opens up before you.")
+print ("> You walk through the portal")
+print ("> You are transported to the far side of this strange portal...a place that feels familiar to you.")
+print ("> You emerge into a lush forest. You feel an incredible sense of relief knowing that you have found your destination.")
+print ("> You look around and notice many different kinds of creatures living in the woods.")
+print ("> You notice that there is a tree that is hollow and you can climb it. The branches are thick and strong.")
+print ("> Before you can look around too much, you hear the crunching and snaping off in the distance of the woods")
+print ("> Before you have time to process what the sound is, a colossal beast breaks through the tree line ")
+print ("> You draw your sword and prepare for battle")
+print ("------------------------------------------------------------------------------------------------------------------------------")
+
+
+#This is the "combat engine" and RNG engine
+
+import random
+
